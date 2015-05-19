5df646facc0a4b2607dd3355c352529ae33b21c3
Stephan Renatus <stephan.renatus@aisec.fraunhofer.de>
2015-05-19 08:36:16 +0200

    CVSS: add temp, fix handling

--- src/Rivum/CVSS.hs
+++ src/Rivum/CVSS.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE NamedFieldPuns #-}
 module Rivum.CVSS where
 
+type Score = Float
+
 -- Base
 data Av = AvL | AvA | AvN deriving (Eq, Show)
 data Ac = AcH | AcM | AcL deriving (Eq, Show)
@@ -45,15 +47,9 @@ defaultBase = Base
     , a  = ImpN
     }
 
-type Score = Float
-
-base :: Base -> Score
-base (Base { av, ac, au, c, i, a }) = ( 0.6 * imp + 0.4 * exploitability - 1.5 ) * (f imp)
+baseByImp :: Float -> Base -> Score
+baseByImp imp (Base { av, ac, au }) = ( 0.6 * imp + 0.4 * exploitability - 1.5 ) * (f imp)
   where
-    imp         = impact confImpact integImpact availImpact
-    confImpact  = fromImp c
-    integImpact = fromImp i
-    availImpact = fromImp a
     exploitability = 20 * accessVector * accessComplexity * authentication
     accessVector     = fromAv av
     accessComplexity = fromAc ac
@@ -62,8 +58,65 @@ base (Base { av, ac, au, c, i, a }) = ( 0.6 * imp + 0.4 * exploitability - 1.5 )
     --round_to_one_decimal x =  (fromInteger $ round $ x * 10) / 10.0
     -- TODO
 
+base :: Base -> Score
+base b@(Base { c, i, a }) = baseByImp imp b
+  where
+    imp         = impact confImpact integImpact availImpact
+    confImpact  = fromImp c
+    integImpact = fromImp i
+    availImpact = fromImp a
+
 impact :: Float -> Float -> Float -> Float
 impact ci ii ai = 10.41 * (1 - (1 - ci) * (1 - ii) * (1 - ai))
+
+-- Temp
+data E = END | EU | EPOC | EF | EH deriving (Eq, Show)
+data Rl = RlND | RlOF | RlTF | RlW | RlU deriving (Eq, Show)
+data Rc = RcND | RcUC | RcUR | RcC deriving (Eq, Show)
+
+fromE :: E -> Float
+fromE END  = 1.0
+fromE EU   = 0.85
+fromE EPOC = 0.9
+fromE EF   = 0.95
+fromE EH   = 1.0
+
+fromRl :: Rl -> Float
+fromRl RlND = 1.0
+fromRl RlOF = 0.87
+fromRl RlTF = 0.9
+fromRl RlW  = 0.95
+fromRl RlU  = 1.0
+
+fromRc :: Rc -> Float
+fromRc RcND = 1.0
+fromRc RcUC   = 0.9
+fromRc RcUR = 0.95
+fromRc RcC  = 1.0
+
+data Temp = Temp
+    { e  :: E
+    , rl :: Rl
+    , rc :: Rc
+    }
+
+defaultTemp = Temp
+    { e  = END
+    , rl = RlND
+    , rc = RcND
+    }
+
+tempByBase :: Score -> Temp -> Score
+tempByBase baseScore (Temp {e, rl, rc}) = baseScore * exploitability * remediationLevel * reportConfidence
+  where
+    exploitability = fromE e
+    remediationLevel = fromRl rl
+    reportConfidence = fromRc rc
+
+temp :: Base -> Temp -> Score
+temp b = tempByBase baseScore
+  where
+    baseScore = base b
 
 -- Env
 data Cdp = CdpND | CdpN | CdpL | CdpLM | CdpMH | CdpH deriving (Eq, Show)
@@ -108,11 +161,11 @@ defaultEnv = Env
     , ar  = ReqND
     }
 
-env :: Base -> Env -> Score
-env b@(Base { c, i, a})  (Env { cdp, td, cr, ir, ar }) = (adjustedTemporal + (10 - adjustedTemporal) * collateralDamagePotential) * targetDistribution
+env :: Base -> Temp -> Env -> Score
+env b@(Base { c, i, a}) t  (Env { cdp, td, cr, ir, ar }) = (adjustedTemporal + (10 - adjustedTemporal) * collateralDamagePotential) * targetDistribution
   where
-    adjustedTemporal = base b -- XXX No proper temporal score handling
-    adjustedImpact = min(10.0, impact (confImpact*confReq) (integImpact*integReq) (availImpact*availReq))
+    adjustedTemporal = tempByBase (baseByImp adjustedImpact b) t
+    adjustedImpact = minimum [10.0, impact (confImpact*confReq) (integImpact*integReq) (availImpact*availReq)]
     collateralDamagePotential = fromCdp cdp
     targetDistribution = fromTd td
     confImpact  = fromImp c
